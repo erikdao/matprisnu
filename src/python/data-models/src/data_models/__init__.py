41aeece03dc02746b2be81b13af483f45259ee5c
erikdao <erik.dao@silo.ai>
2023-04-03 21:07:35 +0200

    🤸 WIP: simplify project structure

--- src/python/data-models/src/data_models/__init__.py
+++ src/python/data-models/src/data_models/__init__.py
@@ -1,5 +1,8 @@
+from importlib.metadata import version
 from .scrappers import (AxfoodAPIProduct, CoopAPICategory, IcaAPICategory,
                         IcaAPIStore)
+
+__version__ = version("data-models")
 
 __all__ = [
     "CoopAPICategory",
