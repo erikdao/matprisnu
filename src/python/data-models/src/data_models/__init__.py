8ff57c16a06c20732d313585cf35afa2aa62096c
erikdao <cuong@neuraltalks.io>
2023-04-04 02:00:24 +0200

    💪 Update: restore desire directory structure

--- src/python/data-models/src/data_models/__init__.py
+++ src/python/data-models/src/data_models/__init__.py
@@ -1,1 +1,5 @@
+from .scrappers import CoopAPICategory, AxfoodAPIProduct, IcaAPIStore, IcaAPICategory
+
 __version__ = "0.1.0"
+
+__all__ = ["CoopAPICategory", "AxfoodAPIProduct", "IcaAPIStore", "IcaAPICategory"]
