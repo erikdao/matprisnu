377d466753ba40d884f2c34ce8072dedfcddafdc
erikdao <cuong@neuraltalks.io>
2023-04-20 03:07:48 +0200

    💪 Add: ingestion function for Ica's categories

--- src/python/data-ingestion/resources/sql/create_categories.sql
+++ src/python/data-ingestion/resources/sql/create_categories.sql
@@ -12,3 +12,15 @@ CREATE TABLE IF NOT EXISTS coop_categories (
     "escapedName" VARCHAR(255),
     "hasChildren" BOOLEAN
 );
+
+CREATE TABLE IF NOT EXISTS ica_categories (
+    "pkey" SERIAL PRIMARY KEY,
+    "scrapped_date" DATE,
+    "updated_at" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    "id" VARCHAR(255) NOT NULL UNIQUE,
+    "name" VARCHAR(255),
+    "retailerId" VARCHAR(255),
+    "children" VARCHAR(255)[],
+    "fullURLPath" VARCHAR(255),
+    "storeId" VARCHAR(255)
+);
--- src/python/data-ingestion/src/data_ingestion/categories.py
+++ src/python/data-ingestion/src/data_ingestion/categories.py
@@ -1,4 +1,6 @@
 """Ingest category data."""
+import json
+import os
 from datetime import datetime
 
 import polars as pl
@@ -13,3 +15,21 @@ def ingest_coop_categories(json_file: str, date: str):
     df = df.with_columns((pl.lit(scrapped_date, dtype=pl.Date).alias("scrapped_date")))
 
     write_to_db(df, table_name="coop_categories")
+
+
+def ingest_ica_categories(json_file: str, date: str):
+    """Ingest ICA categories from JSON file to PostgreSQL table."""
+    with open(json_file, "r") as f:
+        data = json.load(f)
+    categories = list(data.values())
+
+    df = pl.DataFrame(categories)
+
+    scrapped_date = datetime.strptime(date, "%Y-%m-%d")
+    df = df.with_columns((pl.lit(scrapped_date, dtype=pl.Date).alias("scrapped_date")))
+
+    # The convention of json_file is `storeId_cartegories.json`
+    store_id = os.path.basename(json_file).split("_")[0]
+    df = df.with_columns((pl.lit(store_id).alias("storeId")))
+
+    write_to_db(df, table_name="ica_categories")
