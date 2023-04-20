1e9e32506241b1fee09229ccdd4b646850be759a
erikdao <cuong@neuraltalks.io>
2023-04-20 10:13:36 +0200

    🤪 WIP: ingesting categories

--- src/python/data-ingestion/resources/sql/create_categories.sql
+++ src/python/data-ingestion/resources/sql/create_categories.sql
@@ -17,10 +17,34 @@ CREATE TABLE IF NOT EXISTS ica_categories (
     "pkey" SERIAL PRIMARY KEY,
     "scrapped_date" DATE,
     "updated_at" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    "id" VARCHAR(255) NOT NULL UNIQUE,
+    "id" VARCHAR(255) NOT NULL,
     "name" VARCHAR(255),
     "retailerId" VARCHAR(255),
     "children" VARCHAR(255)[],
     "fullURLPath" VARCHAR(255),
     "storeId" VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS hemkop_categories (
+    "pkey" SERIAL PRIMARY KEY,
+    "scrapped_date" DATE,
+    "updated_at" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    "id" VARCHAR(255) NOT NULL,
+    "title" VARCHAR(255),
+    "category" VARCHAR(255),
+    "url" VARCHAR(255),
+    "valid" BOOLEAN,
+    "children" VARCHAR(255)[]
+);
+
+CREATE TABLE IF NOT EXISTS willys_categories (
+    "pkey" SERIAL PRIMARY KEY,
+    "scrapped_date" DATE,
+    "updated_at" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    "id" VARCHAR(255) NOT NULL,
+    "title" VARCHAR(255),
+    "category" VARCHAR(255),
+    "url" VARCHAR(255),
+    "valid" BOOLEAN,
+    "children" VARCHAR(255)[]
 );
--- src/python/data-ingestion/src/data_ingestion/categories.py
+++ src/python/data-ingestion/src/data_ingestion/categories.py
@@ -2,6 +2,7 @@
 import json
 import os
 from datetime import datetime
+from typing import Any, Dict, List
 
 import polars as pl
 from data_ingestion.utils import write_to_db
@@ -33,3 +34,47 @@ def ingest_ica_categories(json_file: str, date: str):
     df = df.with_columns((pl.lit(store_id).alias("storeId")))
 
     write_to_db(df, table_name="ica_categories")
+
+
+def _parse_axfood_categories(data: Any) -> List[Dict[str, Any]]:
+    if len(data["children"]) == 0:
+        return data
+
+    items = []
+    data["children_id"] = [child["id"] for child in data["children"]]
+
+    for child in data["children"]:
+        items.append(_parse_axfood_categories(child))
+
+    return items
+
+
+def _flatten(lst: List[Any]) -> List[Any]:
+    flattened = []
+    for item in lst:
+        if isinstance(item, list):
+            flattened.extend(_flatten(item))
+        else:
+            flattened.append(item)
+    return flattened
+
+
+def _get_flattend_axfood_categories(json_file: str) -> List[Dict[str, Any]]:
+    with open(json_file, "r") as f:
+        data = json.load(f)
+    categories = []
+    for item in data:
+        categories.extend(_flatten(_parse_axfood_categories(item)))
+    return categories
+
+
+def ingest_axfood_categories(brand: str, json_file: str, date: str):
+    """Ingest Axfood categories from JSON file to PostgreSQL table."""
+    # TODO: Fix this, the `children_id` column is not correct
+    categories = _get_flattend_axfood_categories(json_file)
+    df = pl.DataFrame(categories)
+
+    scrapped_date = datetime.strptime(date, "%Y-%m-%d")
+    df = df.with_columns((pl.lit(scrapped_date, dtype=pl.Date).alias("scrapped_date")))
+
+    write_to_db(df, table_name=f"{brand}_categories")
