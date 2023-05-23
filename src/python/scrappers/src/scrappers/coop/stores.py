84190e1d5decd8bf02506fab6d14e45fcc08c98f
erikdao <cuong@neuraltalks.io>
2023-05-23 23:34:09 +0200

    👌Add: Prefect as task runner back

--- src/python/scrappers/src/scrappers/coop/stores.py
+++ src/python/scrappers/src/scrappers/coop/stores.py
@@ -9,12 +9,14 @@ from scrappers.repositories import save_to_document_store, save_to_json
 from tornado.escape import json_decode
 from tornado.httpclient import AsyncHTTPClient, HTTPRequest
 from tornado.httputil import HTTPHeaders
+from prefect import task, flow
 
 load_dotenv(os.path.join(os.getcwd(), ".env"))
 
 BASE_URL = "https://proxy.api.coop.se/external/store/stores/map"
 
 
+@task(name="Scrape Coop stores", tags=["coop", "stores"])
 async def scrape_stores() -> Optional[List[Any]]:
     params = {"conceptIds": "12,6,95", "invertFilter": "true", "api-version": "v2"}
     headers = {
@@ -28,10 +30,12 @@ async def scrape_stores() -> Optional[List[Any]]:
     return json_decode(response.body)
 
 
+@task(name="Save Coop stores to JSON", tags=["coop", "stores"])
 def save_stores_to_json(data):
     save_to_json(data, file_name="stores.json", brand="coop", category="stores")
 
 
+@task(name="Save Coop stores to Document Store", tags=["coop", "stores"])
 async def save_stores_to_document_store(store):
     data = {"data": store}
     await save_to_document_store(
@@ -39,6 +43,7 @@ async def save_stores_to_document_store(store):
     )
 
 
+@flow(name="Scrape Coop stores flow")
 async def scrape_stores_flow():
     stores = await scrape_stores()
     save_stores_to_json(stores)
